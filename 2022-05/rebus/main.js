9d79fb4a83c2d9986f791132bb7b60771131468a
balagur-asteria <99834352+balagur-asteria@users.noreply.github.com>
2022-05-10 13:55:07 +0400

    Add files via upload

--- 2022-05/rebus/main.js
+++ 2022-05/rebus/main.js
@@ -9,117 +9,117 @@
             title: 'Разгадай ребус',
             hash: ['c82609f7d44f77f4f85b7ca66e749346']
         },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: ['491efa07306cc8f7fccad20877cc5cc3']
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: ['829595c37442a945ce101ac9cc3b1d5a']
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: ['5a922eef70cfea05cfce94cfab52f750']
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         'ef9af8192670fd561f692c89c0ced7b4',
-        //         'f91cd01cc73d1e780513a9878fe4e5db',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: ['558cbc0a43bd5759bac3785e7271c9cc']
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         'd3d9c21978b4cd6086a33cc79747da9c',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         '3716035825276d9ffcefb1045febce7a',
-        //         '2879febba0269d45f074db9a00cf05ff',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         '62ced77eb87267c61fa0f9bde73b4c52',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         '1708244516df4c4a9c3d2584a562d683',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         '1ca6e6a0c45d4879973470a1c2956d60',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         '5c3d191fed1b3e14f27dd0f3739d82a8',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         '626d32f809f8f67827475aee02d8b254',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         '7b1e472978ca90cb579a9b4e4c031abe',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         '33fa738c5fcdda8d7ba819d260769506',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         'b2a25130bfd66ccba1c367a5fd6bad1f',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         'be2ed03ac9f701a7e74e1030d7a4e231',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         '632448c24553426a338839a5800c02f9',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         '00e4a3fadbabb3d12098edfc99b44dc2',
-        //     ]
-        // },
-        // {
-        //     title: 'Разгадай ребус',
-        //     hash: [
-        //         '5b88cc77972c21ef175ab5bcdb933d58',
-        //     ]
-        // },
+        {
+            title: 'Разгадай ребус',
+            hash: ['491efa07306cc8f7fccad20877cc5cc3']
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: ['829595c37442a945ce101ac9cc3b1d5a']
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: ['5a922eef70cfea05cfce94cfab52f750']
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                'ef9af8192670fd561f692c89c0ced7b4',
+                'f91cd01cc73d1e780513a9878fe4e5db',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: ['558cbc0a43bd5759bac3785e7271c9cc']
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                'd3d9c21978b4cd6086a33cc79747da9c',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                '3716035825276d9ffcefb1045febce7a',
+                '2879febba0269d45f074db9a00cf05ff',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                '62ced77eb87267c61fa0f9bde73b4c52',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                '1708244516df4c4a9c3d2584a562d683',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                '1ca6e6a0c45d4879973470a1c2956d60',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                '5c3d191fed1b3e14f27dd0f3739d82a8',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                '626d32f809f8f67827475aee02d8b254',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                '7b1e472978ca90cb579a9b4e4c031abe',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                '33fa738c5fcdda8d7ba819d260769506',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                'b2a25130bfd66ccba1c367a5fd6bad1f',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                'be2ed03ac9f701a7e74e1030d7a4e231',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                '632448c24553426a338839a5800c02f9',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                '00e4a3fadbabb3d12098edfc99b44dc2',
+            ]
+        },
+        {
+            title: 'Разгадай ребус',
+            hash: [
+                '5b88cc77972c21ef175ab5bcdb933d58',
+            ]
+        },
     ]
 
-    const timeToStart = 'Sun May 01 2022 18:00:00 GMT+0300';
+    const timeToStart = 'Wed May 11 2022 16:00:00 GMT+0300';
 
     const getTimeRemaining = endTimeString=>{
         const t = Date.parse(endTimeString) - Date.parse(new Date());
